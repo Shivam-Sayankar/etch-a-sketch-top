5ba5b9a3ece304305c96c510b635cea81f953499
Shivam Sayankar <111188660+Shivam-Sayankar@users.noreply.github.com>
2025-08-19 23:11:26 +0530

    Add initial 16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+
+const n = 16;
+let rows = [];
+
+for (let i = 0; i < n; i++) {
+    const row = document.createElement('div')
+    row.classList.add('row-div')
+    container.appendChild(row)
+    rows.push(row)
+}
+
+rows.forEach((row) => {
+
+    for (let i = 0; i < n; i++) {
+        const pixel = document.createElement('div')
+        pixel.classList.add('pixel')
+        row.appendChild(pixel);
+    }
+})
+
