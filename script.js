e1b0b2f3c6af5d03e5cd53ad48d87e461548d065
Shivam Sayankar <111188660+Shivam-Sayankar@users.noreply.github.com>
2025-08-29 01:30:40 +0530

    Make custom event for coloring work     - hover and click options work for coloring     - selecting either clears customisation

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const sketchPad = document.querySelector('.sketch-pad');
+const sketchPad = document.querySelector('.sketch-pad')
+
 const gridSizeRange = document.querySelector('#grid-size-range')
-const colorPicker = document.querySelector('#color-picker')
 const gridSizeSpan = document.querySelector('#grid-size-span')
+
+const colorPicker = document.querySelector('#color-picker')
 const clearButton = document.querySelector('#clear-btn')
+
 const randomColorsCheckbox = document.querySelector('#randomize-checkbox')
 const darkenCheckbox = document.querySelector('#darken-checkbox')
 const gridCheckbox = document.querySelector('#grid-checkbox')
 
-const mainBackgroundColor = "#212121"
+const brushTriggerRadioButtons = document.querySelectorAll('.brush-trigger-radio')
+
+const gridColor = "#212121"
+let brushTriggerEvent = "mouseenter"
 
 function getRandomColors() {
     let colorCode = "#";
@@ -42,18 +48,20 @@ function generateGrid(n) {
         }
     })
 
-    applyColorOnEvent('mouseenter', colorPicker.value);
+    applyColorOnEvent(brushTriggerEvent);
 }
 
-function applyColorOnEvent(trigger, color) {
 
+function setPixelColor(e) {
+    color = colorPicker.value;
+    e.target.style.backgroundColor = color;
+}
+
+function applyColorOnEvent(trigger) {
     let pixels = document.querySelectorAll('.pixel')
     pixels.forEach((pixel) => {
-        pixel.addEventListener(trigger, () => {
-            pixel.style.backgroundColor = color;
-        })
+        pixel.addEventListener(trigger, setPixelColor)
     })
-
 }
 
 
@@ -64,7 +72,7 @@ function decreaseOpacity(e) {
 
 function progressiveDarkening() {
     document.querySelectorAll('.pixel').forEach((pixel) => {
-        pixel.addEventListener('mouseenter', decreaseOpacity)
+        pixel.addEventListener(brushTriggerEvent, decreaseOpacity)
     })
 }
 
@@ -76,7 +84,7 @@ function setRandomBackgroundColor(e) {
 
 function randomColorBrush() {
     document.querySelectorAll('.pixel').forEach((pixel) => {
-        pixel.addEventListener('mouseenter', setRandomBackgroundColor)
+        pixel.addEventListener(brushTriggerEvent, setRandomBackgroundColor)
     })
 }
 
@@ -86,20 +94,19 @@ function clear() {
     pixels.forEach((pixel) => {
         pixel.style.backgroundColor = 'white';
         pixel.style.opacity = 1;
-        pixel.removeEventListener('mouseenter', setRandomBackgroundColor)
-        pixel.removeEventListener('mouseenter', decreaseOpacity)
+        pixel.removeEventListener(brushTriggerEvent, setRandomBackgroundColor)
+        pixel.removeEventListener(brushTriggerEvent, decreaseOpacity)
     })
     randomColorsCheckbox.checked = false;
     darkenCheckbox.checked = false;
 }
 
 randomColorsCheckbox.addEventListener('change', (e) => {
-
     if (e.target.checked) {
         randomColorBrush()
     } else {
         document.querySelectorAll('.pixel').forEach((pixel) => {
-            pixel.removeEventListener('mouseenter', setRandomBackgroundColor)
+            pixel.removeEventListener(brushTriggerEvent, setRandomBackgroundColor)
         })
     }
 })
@@ -109,7 +116,7 @@ darkenCheckbox.addEventListener('change', (e) => {
         progressiveDarkening();
     } else {
         document.querySelectorAll('.pixel').forEach((pixel) => {
-            pixel.removeEventListener('mouseenter', decreaseOpacity)
+            pixel.removeEventListener(brushTriggerEvent, decreaseOpacity)
         })
     }
 })
@@ -117,23 +124,41 @@ darkenCheckbox.addEventListener('change', (e) => {
 gridCheckbox.addEventListener('change', (e) => {
     const pixels = document.querySelectorAll('.pixel')
     if (e.target.checked) {
-        pixels.forEach(pixel => pixel.style.border = `${mainBackgroundColor} 0.1px solid`)
+        pixels.forEach(pixel => pixel.style.border = `${gridColor} 0.1px solid`)
     } else {
         pixels.forEach(pixel => pixel.style.border = 0)
     }
 })
 
 colorPicker.addEventListener('change', (e) => {
-    const currentColor = e.target.value;
-    applyColorOnEvent('mouseenter', currentColor);
+    applyColorOnEvent(brushTriggerEvent);
     randomColorsCheckbox.checked = false;
 });
 
 gridSizeRange.addEventListener('input', (e) => {
     const num = parseInt(e.target.value);
     generateGrid(num);
-    gridSizeSpan.textContent = `${num}x${num}`
+    gridSizeSpan.textContent = `${num} x ${num}`
+    gridCheckbox.checked = true;
 });
+
+brushTriggerRadioButtons.forEach(radio => {
+    radio.addEventListener('change', e => {
+        let oldTriggerEvent = brushTriggerEvent;
+        brushTriggerEvent = e.target.value
+
+        const pixels = document.querySelectorAll('.pixel')
+        pixels.forEach(pixel => {
+            pixel.removeEventListener(oldTriggerEvent, setPixelColor)
+            pixel.removeEventListener(oldTriggerEvent, decreaseOpacity)
+            pixel.removeEventListener(oldTriggerEvent, setRandomBackgroundColor)
+            darkenCheckbox.checked = false;
+            randomColorsCheckbox.checked = false;
+
+            pixel.addEventListener(brushTriggerEvent, setPixelColor)
+        })
+    })
+})
 
 clearButton.addEventListener('click', clear)
 
